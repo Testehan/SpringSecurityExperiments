8fc22186bde970f1cb92e39a80cdf87902acc022
DanTestehan <tdan89@yahoo.com>
2024-05-09 18:36:35 +0300

    chapter 6 config files

---
+++ chapter6/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO users VALUES (1, 'john', '$2a$10$xn3LI/AjqicFYZFruSwve.681477XaVNaUQbr1gioaWPn4t1KsnmG', 'BCRYPT');
+INSERT INTO authorities VALUES (1, 1, 'read');
+INSERT INTO authorities VALUES (2, 1, 'write');
+
+INSERT INTO products VALUES (1, 'Chocolate', '10', 'USD');
