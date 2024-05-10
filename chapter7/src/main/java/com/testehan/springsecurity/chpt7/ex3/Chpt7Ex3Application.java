1bf90d12447c2ddc9d98b4452b3dcfb79bc2b5f8
DanTestehan <tdan89@yahoo.com>
2024-05-10 19:57:36 +0300

    chapter 7 roles example

---
+++ chapter7/src/main/java/com/testehan/springsecurity/chpt7/ex3/Chpt7Ex3Application.java
@@ -0,0 +1,13 @@
+package com.testehan.springsecurity.chpt7.ex3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chpt7Ex3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chpt7Ex3Application.class, args);
+	}
+
+}
