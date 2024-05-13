905c28073b2013710d34788e9ab49dcd9179c12c
DanTestehan <tdan89@yahoo.com>
2024-05-13 17:37:50 +0300

    chapter 10 logging CSRF token

---
+++ chapter10/src/main/java/com/testehan/springsecurity/chpt10/ex2/Chpt10Ex2Application.java
@@ -0,0 +1,17 @@
+package com.testehan.springsecurity.chpt10.ex2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*
+
+*/
+
+
+@SpringBootApplication
+public class Chpt10Ex2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chpt10Ex2Application.class, args);
+	}
+
+}
