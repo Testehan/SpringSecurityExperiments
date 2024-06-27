ad835daa57c580f9c282c9906accca808e660bbb
DanTestehan <tdan89@yahoo.com>
2024-06-27 20:20:20 +0100

    chapter 19 - reactive app - main and config file

---
+++ chapter19/src/main/java/com/testehan/springsecurity/chpt19/Chpt19ReactiveApplication.java
@@ -0,0 +1,21 @@
+package com.testehan.springsecurity.chpt19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+
+	Steps:
+
+
+*/
+
+
+@SpringBootApplication
+public class Chpt19ReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chpt19ReactiveApplication.class, args);
+	}
+
+}
